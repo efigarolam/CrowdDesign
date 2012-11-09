7032dc57938c90f2daacbd5bcc0c9a768d5771f6
Eduardo Figarola <eduardo.figarola@crowdint.com>
2012-11-09 12:55:18 -0600

    Paperclip implemented, now we can upload new 'portfolios' with a title, description, release date and an image, the next step is to modify the view for index action of portfolios to showthe portfolios stored in the db, after that we can apply some style to the administration of portfolios (new, edit and show actions) and then we can limit the access to the portfolio management with some authentication gem like devise, we can follow the example of JSMerchant

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,4 @@
 class Portfolio < ActiveRecord::Base
-  attr_accessible :description, :release_date, :title
+  attr_accessible :description, :release_date, :title, :image
+  has_attached_file :image, :styles => { :medium => "320x205>", :thumb => "100x100>" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121107123730) do
+ActiveRecord::Schema.define(:version => 20121109181454) do
 
   create_table "portfolios", :force => true do |t|
     t.string   "title"
     t.string   "description"
     t.date     "release_date"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
